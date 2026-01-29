9881e7f7085f2d1e6f10f87bab9491532b5f159c
Herlan <herlanrodriguez009@gmail.com>
2026-01-28 22:15:49 -0300

    Refactor API endpoints for student management and add sample server data

--- api_escola/index.js
+++ api_escola/index.js
@@ -18,33 +18,74 @@ app.use(express.json());
 
 app.get("/buscar", async (request, response) => {
 
-    const data = await db("ALUNOS").select();
+    const {id} = request.query;
 
-    console.log(data);
+    var data = [];
 
-    response.send("Bem-vindo à API da Escola!");
+try {
+    if (id) {
+        data = await db("ALUNOS").select().where({ id });
+    } else {
+        data = await db("ALUNOS").select();
+    }
+
+    response.send(data);
+} catch (error) {
+    console.log(error);
+    response.status(500).send("Erro ao buscar alunos."); 
 });
 
 app.post("/cadastrar", async (request, response) => {
-    const { nome, ra } = request.body;
+    const { id, nome, ra } = request.body;
     
-    const data = await db("ALUNOS").insert({ nome, ra });
+    const data = await db("ALUNOS").insert({ id, nome, ra });
 
     if (data.length >0 ){
         response.send("Aluno cadastrado com sucesso!");
     } else {
-        response.send("Erro ao cadastrar aluno.");
+        response.status(500).send("Erro ao cadastrar aluno.");
     }
 
     response.send({ nome, ra });
 });
 
 
-app.put("/atualizar/:ra", (request, response) => {
-    response.send("Aluno atualizado com sucesso!");
+app.put("/atualizar/:id", async (request, response) => {
+    const { id } = request.params;
+    const { nome, ra, status } = request.body
+
+
+    if (!Number.isInteger(Number(id))){
+        response.send ({msg: "O parametro ID precisa ser um número inteiro."});
+    }
+
+    const data = await db("ALUNOS").update ({ 
+        nome, 
+        ra,
+        status
+    }).where({ id });
+
+    if (data == 1) {
+        response.send({msg:"Aluno atualizado com sucesso!"});
+    } else {
+        response.send({msg:"Erro ao atualizar aluno."});
+    }
+
+    response.send({id, nome, ra, status});
+    
 });
 
-app.delete("/deletar/:ra", (request, response) => {
+app.delete("/deletar/:id", async(request, response) => {
+
+    const { id } = request.params;
+
+    const data = await db ("ALUNOS").delete().where({ id });
+
+    if (data == 1) {
+        response.send("Aluno deletado com sucesso!");
+    } else {
+        response.send("Erro ao deletar aluno.");
+    }
     response.send("Aluno deletado com sucesso!");
 });
 
---
+++ api_escola/para o servidor.js
@@ -0,0 +1,25 @@
+//porta servidor 
+
+// http://localhost:8080/buscar
+
+
+
+//SERVER 
+{
+    "id": "1",
+    "nome": "MATHEUSINHO MOLE",
+    "ra": "6969",
+    "status": true
+}
+{
+    "id": "2",
+    "nome": "METHEUS MOLENGO",
+    "ra": "9696",
+    "status": true
+}
+{
+    "id": "3",
+    "nome": "MATHIAS FLAMENGO",
+    "ra": "9999",
+    "status": true
+}
